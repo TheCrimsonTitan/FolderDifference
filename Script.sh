ebf88659ee906f1ab19334ff8831382dc6150ca9
TheCrimsonTitan <antoniolansang500@gmail.com>
2023-12-28 01:53:15 -0600

    Something is going wrong with the differences in files

--- Script.sh
+++ Script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 
-echo "hello"
+ls '/mnt/g/My Drive/SSD MediaVault/d'>> FoldersInGDrive.txt
+ls '/mnt/f/PC MediaVault/d' >> FoldersInPCVault.txt
+
+diff FoldersInGDrive.txt FoldersInPCVault.txt >> FoldersToUpload.txt
+grep ">" FoldersToUpload.txt > FoldersToUpload22.txt
+
 
